6861186d6901e58267ff6954c96728d748803afc
Artur Prepelytsia <preart81@gmail.com>
2024-09-22 01:18:07 +0300

    5. save to localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import ContactForm from './components/ContactForm/ContactForm';
 import SearchBox from './components/SearchBox/SearchBox';
 import ContactList from './components/ContactList/ContactList';
 
 function App() {
-  const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
+  const [contacts, setContacts] = useState(() => {
+    const savedContacts = localStorage.getItem('contacts');
+
+    if (savedContacts !== null && savedContacts !== '[]')
+      return JSON.parse(savedContacts);
+    else
+      return [
+        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      ];
+  });
   const addContact = newContact => {
     setContacts(prevContacts => [...prevContacts, newContact]);
   };
@@ -20,8 +27,12 @@ function App() {
     );
   };
 
-  const [searchText, setSetsearchText] = useState('');
-  const filter = searchText.toLowerCase().trim();
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const [searchText, setSearchText] = useState('');
+  const filter = searchText.toLowerCase().trim() || '';
   const filteredContacts = contacts.filter(
     contact =>
       contact.name.toLowerCase().includes(filter) ||
@@ -32,7 +43,7 @@ function App() {
       <div>
         <h1>Phonebook</h1>
         <ContactForm addContact={addContact} />
-        <SearchBox searchText={searchText} handleSearch={setSetsearchText} />
+        <SearchBox searchText={searchText} handleSearch={setSearchText} />
         <ContactList
           contacts={filteredContacts}
           deleteContact={deleteContact}
